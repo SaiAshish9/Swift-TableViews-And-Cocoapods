107c728f964dc6b63560c99653e738bf4722163a
saiashish9 <saiashish3760@gmail.com>
2021-06-27 22:19:11 +0530

    completed

---
+++ Flash Chat iOS13/Models/Message.swift
@@ -0,0 +1,14 @@
+//
+//  messages.swift
+//  Flash Chat iOS13
+//
+//  Created by Sai Ashish Darapureddy on 27/06/21.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+}
